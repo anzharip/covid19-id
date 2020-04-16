2915d4d7942f6102e3e2c79502c041a8ed234e20
anzhari.p@gmail.com <anzhari.p@gmail.com>
2020-04-16 10:35:44 +0700

    Revamping scraper using selenium, and moving the url to Kemenkes's site

--- scrape.py
+++ scrape.py
@@ -1,57 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+
 import requests
-from lxml import html
 import json
 import datetime
 
-url = 'https://www.covid19.go.id'
+url = 'https://www.kemkes.go.id'
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+browser = webdriver.Chrome(options=chrome_options)
+browser.get(url)
+delay = 60  # seconds
 
-try:    
-    pageContent = requests.get(url)
-except requests.ConnectionError as e:
-    print(e)
+
+try:
+    myElem = WebDriverWait(browser, delay).until(
+        EC.visibility_of_element_located((By.CLASS_NAME, 'covid-case-container')))
+    print("Page is ready!")
+except TimeoutException:
+    print("Loading took too much time!")
 finally:
-    tree = html.fromstring(pageContent.content)
-
-indonesia_positif = tree.xpath(
-    '//*[@id="post-2334"]/div/div[6]/div/div[2]/div/div[1]/p[2]/strong/text()')
-indonesia_sembuh = tree.xpath(
-    '//*[@id="post-2334"]/div/div[6]/div/div[2]/div/div[1]/p[3]/strong/text()')
-indonesia_meninggal = tree.xpath(
-    '//*[@id="post-2334"]/div/div[6]/div/div[2]/div/div[1]/p[4]/strong/text()')
-
-global_negara_kawasan = tree.xpath(
-    '//*[@id="post-2334"]/div/div[6]/div/div[1]/div/div[1]/p[2]/strong/text()')
-global_kasus_terkonfirmasi = tree.xpath(
-    '//*[@id="post-2334"]/div/div[6]/div/div[1]/div/div[1]/p[3]/strong/text()')
-global_kematian = tree.xpath(
-    '//*[@id="post-2334"]/div/div[6]/div/div[1]/div/div[1]/p[4]/strong/text()')
+    indonesia_positif = browser.find_element_by_xpath(
+        '//*[@id="vbMainLayer"]/div[6]/div/ul/li[5]/table/tbody/tr[1]/td[3]').text
+    indonesia_sembuh = browser.find_element_by_xpath(
+        '//*[@id="vbMainLayer"]/div[6]/div/ul/li[5]/table/tbody/tr[2]/td[3]').text
+    indonesia_meninggal = browser.find_element_by_xpath(
+        '//*[@id="vbMainLayer"]/div[6]/div/ul/li[5]/table/tbody/tr[3]/td[3]').text
+    print("indonesia_positif: {}".format(indonesia_positif))
+    print("indonesia_sembuh: {}".format(indonesia_sembuh))
+    print("indonesia_meninggal: {}".format(indonesia_meninggal))
 
 covid_data = {
     "Indonesia": {
-        "Positif": int(indonesia_positif[0].replace(",", "")),
-        "Sembuh": int(indonesia_sembuh[0].replace(",", "")),
-        "Meninggal": int(indonesia_meninggal[0].replace(",", "")),
+        "Positif": int(indonesia_positif.replace(",", "")),
+        "Sembuh": int(indonesia_sembuh.replace(",", "")),
+        "Meninggal": int(indonesia_meninggal.replace(",", "")),
     },
-    "Global": {
-        "Negara/Kawasan": int(global_negara_kawasan[0].replace(",", "")),
-        "Kasus Terkonfirmasi": int(global_kasus_terkonfirmasi[0].replace(",", "")),
-        "Kematian": int(global_kematian[0].replace(",", "")),
-    },
-    "Timestamp": str(datetime.datetime.now()),
+    "Date Retrieved": str(datetime.datetime.now()),
     "Source": url,
 
 }
-
-print('indonesia_positif: {}'.format(indonesia_positif))
-print('indonesia_sembuh: {}'.format(indonesia_sembuh))
-print('indonesia_meninggal: {}'.format(indonesia_meninggal))
-
-print('global_negara_kawasan: {}'.format(global_negara_kawasan))
-print('global_kasus_terkonfirmasi: {}'.format(global_kasus_terkonfirmasi))
-print('global_kematian: {}'.format(global_kematian))
 
 print('covid_data: {}'.format(covid_data))
 
 # Write to json file
 with open('covid_data.json', 'w') as outfile:
     json.dump(covid_data, outfile)
+
+browser.close()
